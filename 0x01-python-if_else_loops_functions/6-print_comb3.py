91694eadfcc9c10cbdd0a41357373f0e0eab21b5
root <admasterefe00@gmail.com>
2022-05-31 12:42:12 -0700

    saved changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i < j:
-            print(f"{i}{j},", end=' ')
+            if i == 8 and j == 9:
+                print(89)
+                break
+            print("{}{},".format(i, j), end=' ')
