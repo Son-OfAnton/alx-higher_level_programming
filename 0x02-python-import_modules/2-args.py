6bb74d2810c41a4a245218dabd9eb4cc0efc47ed
root <admasterefe00@gmail.com>
2022-06-02 11:30:24 -0700

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-n = len(sys.argv)
+n = len(sys.argv) - 1
 
-if n == 2:
+if n == 1:
     print("1 argument:")
-elif n == 1:
+elif n == 0:
     print("0 arguments:")
 else:
     print("{} arguments:".format(n))
-for i in range(1, n):
+for i in range(1, n + 1):
     print("{}: {}".format(i, sys.argv[i]))
