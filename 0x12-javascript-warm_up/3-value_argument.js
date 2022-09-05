cdc005e6c878c5c2120d4c418e243f230406c363
Son-ofAnton <aadmasterefe00@gmail.com>
2022-09-05 16:08:07 +0300

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const args = process.argv[2];
-if (args === undefined)
-{
-    console.log('No argument');
-} else
-{
-    console.log(args);
+if (args === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args);
 }
