a2f2cbb9ecc7b37ad3cc4e6cadcd2d28861393f2
Son-ofAnton <aadmasterefe00@gmail.com>
2022-09-05 15:51:33 +0300

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-
 const args = process.argv;
 if (args.length - 2 === 0)
+{
     console.log('No argument');
-else if (args.length - 2 === 1)
+} else if (args.length - 2 === 1)
+{
     console.log('Argument found');
-else
+} else
+{
     console.log('Arguments found');
+}
